08f3e95e9d3b4594725861128e7e36a0252834e0
Awais <waizzy444@gmail.com>
2022-01-21 15:45:57 +0500

    create employee model

---
+++ server/models/employee.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    employeePicUrl: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    employeePicName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    employeeName: {
+        type: String,
+        required: true
+    },
+    employeeDesignation: {
+        type: String,
+        required: true
+    },
+    employeeGrossSalary: {
+        type: Number,
+        required: true
+    },
+    employeeNetSalary: {
+        type: Number,
+        required: true
+    },
+    employeeTaxes: {
+        type: Number,
+        required: true
+    },
+    employeeExperience: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Experiences'
+    },
+    employeeRole: {
+        type: String,
+        required: true
+    },
+    employeeStatus: {
+        type: String,
+        required: true
+    },
+    employeeDepartment: {
+        type: String,
+        required: true
+    }
+},
+{
+    timestamps:true
+})
+
+const Employee = mongoose.model('Employee',employeeSchema)
+module.exports = Employee
