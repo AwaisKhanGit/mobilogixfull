19035a21ea6abf836880842c1b66fbcf0def314c
Awais <waizzy444@gmail.com>
2022-01-21 14:56:02 +0500

    add redux store

---
+++ server/db/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+module.exports = connectDB = async () =>{
+    try {
+        const connect = await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify:false,
+            useUnifiedTopology: true
+        })
+
+        console.log(`MongoDB Connected : ${connect.connection.host}`)
+    } catch (error) {
+        console.log(`Error Connecting Mongo: ${error.message}`)
+        process.exit(1)
+    }
+}
+
